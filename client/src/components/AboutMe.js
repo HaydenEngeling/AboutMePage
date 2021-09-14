9eb11a18fea2be1fff1f91f00a4682aed89e311b
Hayden Engeling <haydentoddengeling@gmail.com>
2021-09-14 10:17:11 -0500

    about me update

--- client/src/components/AboutMe.js
+++ client/src/components/AboutMe.js
@@ -1,9 +1,23 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+import Divider from '@material-ui/core/Divider';
+import profile from '../HaydenPhoto.jpeg';
 
 const useStyles = makeStyles({
   root: {
     backgroundColor: '#A2CBC9'
+  },
+  profile: {
+    maxWidth: '100%',
+    display: 'flex',
+    maxHeight: 500,
+
+  },
+  paper: {
+    padding: 10,
+    margin: 10
   }
 })
 
@@ -11,6 +25,30 @@ const AboutMe = () => {
   const classes = useStyles();
   return (
     <div className={classes.root}>
+      <Grid container
+        justifyContent='center'
+        alignItems='center'
+      >
+        <Grid item xs={4}>
+          <Paper className={classes.paper} elevation={3} >
+            <Grid container justifyContent='center'>
+              <img
+                alt=''
+                src={profile}
+                className={classes.profile}
+              />
+            </Grid>
+          </Paper>
+        </Grid>
+        <Grid item xs>
+          <Divider orientation="vertical" />
+        </Grid>
+        <Grid item xs={6}>
+          <Paper className={classes.paper}>
+
+          </Paper>
+        </Grid>
+      </Grid>
       AboutMe
     </div>
   )
